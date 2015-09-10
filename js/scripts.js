fe22cb0ad3b8dea991a22d1d726aea3e29d0d1e0
Ian McKenney & Samantha Maturen <student@epicodus.com>
2015-09-10 16:46:37 -0700

    add error preventing logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
 var wordOrder = function(userString) {
-    //removes special characters and convert to lowercase
-    userString = userString.replace(/[^a-zA-Z" "]+/g, '').toLowerCase();
-    //removes extra whitespace
-    var splitString = userString.replace(/(\s+\s+)/g, ' ').split(' ');
     var wordCount = {};
-    var finalWordCount = {};
-    for (var i = 0; i < splitString.length; i++) {
-        if (splitString[i] in wordCount) {
-            wordCount[splitString[i]] += 1;
-        } else {
-            wordCount[splitString[i]] = 1;
+    //removes special characters and convert to lowercase
+    userString = userString.trim().replace(/[^a-zA-Z" "]+/g, '').toLowerCase();
+    //removes extra whitespace
+    userString = userString.replace(/(\s+\s+)/g, ' ')
+    if ((userString !== '') && (userString !== ' ')) {
+        var splitString = userString.split(' ');
+        var finalWordCount = {};
+        for (var i = 0; i < splitString.length; i++) {
+            if (splitString[i] in wordCount) {
+                wordCount[splitString[i]] += 1;
+            } else {
+                wordCount[splitString[i]] = 1;
+            }
         }
-    }
-    debugger;
-    var wordCountSorted = Object.keys(wordCount).sort(function(a,b){return wordCount[b] - wordCount[a]});
-    for (var i = 0; i < wordCountSorted.length; i++){
-        finalWordCount[wordCountSorted[i]] = wordCount[wordCountSorted[i]]; ;
-    }
+        var wordCountSorted = Object.keys(wordCount).sort(function(a,b){return wordCount[b] - wordCount[a]});
+        for (var i = 0; i < wordCountSorted.length; i++){
+            finalWordCount[wordCountSorted[i]] = wordCount[wordCountSorted[i]]; ;
+        }
 
-    return finalWordCount;
+        return finalWordCount;
+    } else {
+        wordCount["error"] = "enter some words!";
+        return wordCount;
+    }
 };
 
 $(document).ready(function() {
@@ -26,7 +31,7 @@ $(document).ready(function() {
         var result = wordOrder($("input#userString").val());
         $('.wordOrder').empty();
         for (key in result) {
-            $('.wordOrder').append(key + ": " + result[key] + " ");
+            $('.wordOrder').append(key + ": " + result[key] + " " + "<br>");
         }
         event.preventDefault();
     });
